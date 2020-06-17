b21fea8b06465cb6bd67fdaee39bb8e3e039313b
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-17 11:51:47 -0700

    can identify spaces 1-9 on game board during click events
    Co-authored-by: Matt McFarland <mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,21 @@ function Player() {
 function Board() {
   this.gameBoard = document.getElementById('gameBoard');
   this.gameBoardCtx = gameBoard.getContext('2d');
+  this.row1 = 100;
+  this.row2 = 200;
+  this.row3 = 300;
+  this.col1 = 100;
+  this.col2 = 200;
+  this.col3 = 300;
+  // this.space1 = { xPos: 100, yPos: 100 }
+  // this.space2 = { xPos: 200, yPos: 100 }
+  // this.space3 = { xPos: 300, yPos: 100 }
+  // this.space4 = { xPos: 100, yPos: 200 }
+  // this.space5 = { xPos: 200, yPos: 200 }
+  // this.space6 = { xPos: 300, yPos: 200 }
+  //define the game spaces
+  // compare spaceclick to the gamespaces
+  // function to draw an x or o in the gamespace that the click occurred
 }
 
 Board.prototype.drawBoard = function() {
@@ -43,8 +58,43 @@ Board.prototype.listener = function() {
       xPos: event.layerX,
       yPos: event.layerY
     }
-    playerAction(spaceClicked);
+    newGameBoard.spaceChecker(spaceClicked);
   })
+}
+
+Board.prototype.spaceChecker = function(spaceClicked) {
+  if (spaceClicked.yPos <= this.row1) {
+    if (spaceClicked.xPos <= this.col1) {
+      console.log("space 1");
+    } else if (spaceClicked.xPos <= this.col2) {
+      console.log("space 2");
+    } else if (spaceClicked.xPos <= this.col3) {
+      console.log("space 3")
+    }
+  } else if (spaceClicked.yPos <= this.row2) {
+    if (spaceClicked.xPos <= this.col1) {
+      console.log("space 4");
+    } else if (spaceClicked.xPos <= this.col2) {
+      console.log("space 5");
+    } else if (spaceClicked.xPos <= this.col3) {
+      console.log("space 6")
+    }
+  } else if (spaceClicked.yPos <= this.row3) {
+    if (spaceClicked.xPos <= this.col1) {
+      console.log("space 7");
+    } else if (spaceClicked.xPos <= this.col2) {
+      console.log("space 8");
+    } else if (spaceClicked.xPos <= this.col3) {
+      console.log("space 9")
+    }
+  }
+  // if (spaceClicked.xPos <= this.space1.xPos && spaceClicked.yPos <= this.space1.yPos) {
+  //   console.log('Space 1 clicked!')
+  // } else if (spaceClicked.xPos <= this.space2.xPos && spaceClicked.yPos <= this.space1.yPos) {
+  //   console.log('Space 2 clicked!')
+  // } else if (spaceClicked.xPos <= this.space3.xPos && spaceClicked.yPos <= this.space1.yPos) {
+  //   console.log('Space 3 clicked!')
+  // }
 }
 
 function playerAction(spaceClicked) {
